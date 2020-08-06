5b3e0a9f9ec864b45ece323833bfdd87b9357e23
AmitDevBol <-list>
2020-08-06 12:24:25 +0530

    Initial Commit

---
+++ Beta3+/Beta3_App.swift
@@ -0,0 +1,17 @@
+//
+//  Beta3_App.swift
+//  Beta3+
+//
+//  Created by dominator on 06/08/20.
+//
+
+import SwiftUI
+
+@main
+struct Beta3_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Beta3+/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Beta3+
+//
+//  Created by dominator on 06/08/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
